2038d482485de209ed59763679361a48931f7f6b
Bashir Okala <bashirokala@hotmail.com>
2024-03-21 04:24:08 +0100

    fixed issue with node server

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 // Serve static files from the 'dist' directory
-app.use(express.static(__dirname + '/dist/qr-code-demo'));
+app.use(express.static(path.join(__dirname, 'dist/qr-code-demo/browser')));
 
 // Define a catch-all route to serve the 'index.html' file
+// Define a catch-all route to serve the 'index.html' file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/qr-code-demo/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/qr-code-demo/browser/index.html'));
 });
-
 const server = http.createServer(app);
 
 server.listen(port, () => console.log(`App running on: http://localhost:${port}`));
